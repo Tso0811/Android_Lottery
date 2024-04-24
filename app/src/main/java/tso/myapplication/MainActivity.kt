b462d41f876cd60b9a1478998c9ec21d72d274db
Tso0811 <savagetso0811@gmail.com>
2024-04-24 17:39:18 +0800

    第二次commit 加入樂透功能

--- app/src/main/java/tso/myapplication/MainActivity.kt
+++ app/src/main/java/tso/myapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package tso.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var txv: TextView = findViewById(R.id.number)
+        txv.text = "0"
+
+    }
+    fun main(v: View){
+        var txv: TextView = findViewById(R.id.number)
+        txv.text = (1..100).random().toString()
     }
 }
